f28568e54e6c2e0c715175a06cf733dae0e12c4a
charlie763 <cwisoff@gmail.com>
2020-09-01 16:36:01 -0600

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,63 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(recordAry){
+    return {
+      firstName: recordAry[0],
+      familyName: recordAry[1],
+      title: recordAry[2],
+      payPerHour: recordAry[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+  
+  function createEmployeeRecords(recordsAry){
+    return recordsAry.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateStamp){
+    return createTimeEvent.call(this, "timeIn", dateStamp);
+  }
+  
+  function createTimeOutEvent(dateStamp){
+    return createTimeEvent.call(this, "timeOut", dateStamp);
+  }
+  
+  function createTimeEvent(type, dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+    this[type + "Events"].push({
+      type: type[0].toUpperCase() + type.slice(1),
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+    return this
+  }
+  
+  function hoursWorkedOnDate(date){
+    const timeInEvent = this.timeInEvents.find((event)=>event.date === date)
+    const timeOutEvent = this.timeOutEvents.find((event)=>event.date === date)
+    return (timeOutEvent.hour - timeInEvent.hour)/100;
+  }
+  
+  function wagesEarnedOnDate(date){
+    return this.payPerHour*hoursWorkedOnDate.call(this, date);
+  }
+  
+//   function allWagesFor(record){
+//     const datesWorked = record.timeOutEvents.map((event)=>event.date)
+//     return datesWorked.reduce((totalWages, date)=> {
+//       return totalWages + wagesEarnedOnDate(record, date)
+//     }, 0)
+//   }
+  
+  function findEmployeeByFirstName(recordsAry, firstName){
+    return recordsAry.find((record)=>record.firstName === firstName)
+  }
+  
+  function calculatePayroll(recordsAry){
+    return recordsAry.reduce(function (totalWages, record){
+      return totalWages + allWagesFor.call(record);
+    }, 0);
+  }
