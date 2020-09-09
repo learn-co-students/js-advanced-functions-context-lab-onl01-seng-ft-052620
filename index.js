abbcf30e171709dcbfebb565170da4dbb7fb945d
SWFullPotential <swfullpotential@gmail.com>
2020-09-08 22:04:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    let employee = { 
+        firstName: array[0], 
+        familyName: array[1], 
+        title: array[2], 
+        payPerHour: array[3], 
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+function createEmployeeRecords(array){
+    let employees = array.map(e => createEmployeeRecord(e))
+    return employees
+}
 
+function createTimeInEvent(timeStamp){
+    this.timeInEvents.push({
+        type: "TimeIn", 
+        date: timeStamp.split(" ")[0],
+        hour: parseInt(timeStamp.split(" ")[1], 10)
+    })
+    return this 
+}
+function createTimeOutEvent(timeStamp){
+    this.timeOutEvents.push({
+        type: "TimeOut", 
+        date: timeStamp.split(" ")[0],
+        hour: parseInt(timeStamp.split(" ")[1], 10)
+    })
+    return this
+}
+function hoursWorkedOnDate(timeStamp){
+    let timeInOnDate = this.timeInEvents.find(timeIn => timeIn.date === timeStamp)
+    let timeOutOnDate = this.timeOutEvents.find(timeOut => timeOut.date === timeStamp)
+    let hoursWorked = timeOutOnDate.hour - timeInOnDate.hour 
+    return hoursWorked/100 
+}
+function wagesEarnedOnDate(timeStamp){
+    let wagesEarned = hoursWorkedOnDate.call(this, timeStamp)
+    return wagesEarned * this.payPerHour
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,3 +61,12 @@ let allWagesFor = function () {
 
     return payable
 }
+function findEmployeeByFirstName(srcArray, name){
+    return srcArray.find(e => e.firstName === name)
+}
+function calculatePayroll(array){
+    return array.reduce(function(memo, record){
+        return memo + allWagesFor.call(record)
+    }, 0)
+}
+
